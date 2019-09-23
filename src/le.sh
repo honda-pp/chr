667ca99a1543d79f39f8121403a473e298b21f12
honda <honda@ykw.elec.keio.ac.jp>
2019-09-23 15:55:13 +0900

    auto

---
+++ src/le.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+env="MountainCar-v0"
+mx=20
+for ((i=0;i<$mx;i+=2));do
+    echo ${i}"cheet0"
+    nohup python3 -u pa_vc.py --outdir m/cheet --env $env --gpu 0 --seed $i > vc0.out
+done&
+for ((i=1;i<$mx;i+=2));do
+    echo ${i}"cheet1"
+    nohup python3 -u pa_vc.py --outdir m/cheet --env $env --gpu 1 --seed $i > vc1.out
+done&
+for ((i=0;i<$mx;i+=2));do
+    echo ${i}"norm0"
+    nohup python3 -u pa.py --outdir m/norm --env $env --gpu 0 --seed $i> nm0.out
+    
+done&
+for ((i=1;i<$mx;i+=2));do
+    echo ${i}"norm1"
+    nohup python3 -u pa.py --outdir m/norm --env $env --gpu 1 --seed $i> nm1.out
+done;

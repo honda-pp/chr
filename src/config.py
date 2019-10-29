7d5d32ee13d38518c7d26861fc375c034c094146
honda <honda@ykw.elec.keio.ac.jp>
2019-10-29 14:54:45 +0900

    arg

--- src/config.py
+++ src/config.py
@@ -30,27 +30,30 @@ class A2CFFSoftmax(chainer.ChainList, a2c.A2CModel):
     def pi_and_v(self, state):
         return self.pi(state), self.v(state)
 
-def agp(parser=argparse.ArgumentParser()):    
-    parser.add_argument('--env', type=str, default='CartPole-v0')
-    parser.add_argument('--seed', type=int, default=0,
+def agp(parser=argparse.ArgumentParser(), env='CartPole-v0', seed=0, outdir=None, profile='store_true', steps=2*10**5, 
+        update_steps=5, log_interval=10**3, eval_interval=10**3, eval_n_runs=14, reward_scale_factor=1e-2, gamma=0.99, 
+        lr=7e-4, gpu=1,num_envs=8
+    ):    
+    parser.add_argument('--env', type=str, default=env)
+    parser.add_argument('--seed', type=int, default=seed,
                         help='Random seed [0, 2 ** 32)')
-    parser.add_argument('--outdir', type=str, default=None)
-    parser.add_argument('--profile', action='store_true')
-    parser.add_argument('--steps', type=int, default=2 * 10 ** 5)
-    parser.add_argument('--update-steps', type=int, default=5)
-    parser.add_argument('--log-interval', type=int, default=10 ** 3)
-    parser.add_argument('--eval-interval', type=int, default=10 ** 3)
-    parser.add_argument('--eval-n-runs', type=int, default=14)
-    parser.add_argument('--reward-scale-factor', type=float, default=1e-2)
+    parser.add_argument('--outdir', type=str, default=outdir)
+    parser.add_argument('--profile', action=profile)
+    parser.add_argument('--steps', type=int, default=steps)
+    parser.add_argument('--update-steps', type=int, default=update_steps)
+    parser.add_argument('--log-interval', type=int, default=log_interval)
+    parser.add_argument('--eval-interval', type=int, default=eval_interval)
+    parser.add_argument('--eval-n-runs', type=int, default=eval_n_runs)
+    parser.add_argument('--reward-scale-factor', type=float, default=reward_scale_factor)
     parser.add_argument('--rmsprop-epsilon', type=float, default=1e-5)
     parser.add_argument('--render', action='store_true', default=False)
-    parser.add_argument('--gamma', type=float, default=0.99,
+    parser.add_argument('--gamma', type=float, default=gamma,
                         help='discount factor')
     parser.add_argument('--use-gae', action='store_true', default=False,
                         help='use generalized advantage estimation')
     parser.add_argument('--tau', type=float, default=0.95,
                         help='gae parameter')
-    parser.add_argument('--lr', type=float, default=7e-4)
+    parser.add_argument('--lr', type=float, default=lr)
     parser.add_argument('--weight-decay', type=float, default=0.0)
     parser.add_argument('--demo', action='store_true', default=False)
     parser.add_argument('--load', type=str, default='')
@@ -60,7 +63,7 @@ def agp(parser=argparse.ArgumentParser()):
                         help='value loss coefficient')
     parser.add_argument('--alpha', type=float, default=0.99,
                         help='RMSprop optimizer alpha')
-    parser.add_argument('--gpu', '-g', type=int, default=1,
+    parser.add_argument('--gpu', '-g', type=int, default=gpu,
                         help='GPU ID (negative value indicates CPU)')
-    parser.add_argument('--num-envs', type=int, default=8)
+    parser.add_argument('--num-envs', type=int, default=num_envs)
     return  parser.parse_args()

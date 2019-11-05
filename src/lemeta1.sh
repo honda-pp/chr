0b024b29e8daa323ea3357e648c19215ba9bcd74
honda-keio <honda@ykw.elec.keio.ac.jp>
2019-11-05 12:38:25 +0900

    auto

---
+++ src/lemeta1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+env0="CartPole-v0"
+mx=20
+vepochs=4
+innerepochs=4
+name="c/meta-vepoch"${vepochs}"-inepochs"${innerepochs}
+N=4
+for ((s=0;s<$N;s+=1));do
+    for ((i=$s;i<$mx;i+=4));do
+        echo ${i}${name}
+        python3 -u pa_meta.py --outdir $name --env $env0 --gpu $(($i%2)) --seed $i --v_learn_epochs $vepochs --innerepochs $innerepochs;
+    done&
+done;

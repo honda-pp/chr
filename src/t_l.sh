589a08afab454ab1c75b01af0e4680b68f5905e9
honda <honda@ykw.elec.keio.ac.jp>
2019-11-05 14:05:42 +0900

    for condition

--- src/t_l.sh
+++ src/t_l.sh
@@ -4,9 +4,9 @@ mx=1000
 innerepochs=4
 t_v_learn_epochs=200
 N=4
-for ((s=1;s<=$N;s+=1));do
-    for ((i=$s;i<$mx;i+=4));do
+for ((s=0;s<$N;s+=1));do
+    for ((i=$s;i<$mx;i+=$N));do
         echo ${i}${name}
-        python3 -u meta_opt.py --env $env --gpu $(($i%2)) --t $[i*s] --innerepochs $innerepochs --t_v_learn_epochs $t_v_learn_epochs > "log/out"${i}".txt";
+        python3 -u meta_opt.py --env $env --gpu $(($i%2)) --t $i --innerepochs $innerepochs --t_v_learn_epochs $t_v_learn_epochs > "log/out"${i}".txt";
     done&
 done;

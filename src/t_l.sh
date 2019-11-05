557b3d190a0f9d6ccd617c7a3b97e39f3a861975
honda <honda@ykw.elec.keio.ac.jp>
2019-11-05 14:00:18 +0900

    epoch

--- src/t_l.sh
+++ src/t_l.sh
@@ -2,10 +2,11 @@
 env="CartPole-v0"
 mx=1000
 innerepochs=4
-N=8
+t_v_learn_epochs=200
+N=4
 for ((s=1;s<=$N;s+=1));do
     for ((i=$s;i<$mx;i+=4));do
         echo ${i}${name}
-        python3 -u meta_opt.py --env $env --gpu $(($i%2)) --t $[i*s] --innerepochs $innerepochs;
+        python3 -u meta_opt.py --env $env --gpu $(($i%2)) --t $[i*s] --innerepochs $innerepochs --t_v_learn_epochs $t_v_learn_epochs; 
     done&
 done;

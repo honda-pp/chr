dba6f9bdab6a1bb1d97f365f8d0c40e9417bf563
honda <honda@ykw.elec.keio.ac.jp>
2020-01-07 12:54:17 +0900

    fix:sve model

--- src/save_models.py
+++ src/save_models.py
@@ -2,7 +2,7 @@ from config import *
 import logging, time, os
 import numpy as np
 from chainerrl.experiments import evaluator
-from chainer import serializers
+#from chainer import serializers
 
 outnum = 0
 
@@ -18,7 +18,8 @@ def batch_run_evaluation_episodes(
     global outnum
     logger = logger or logging.getLogger(__name__)
     outdir = logger.name
-    serializers.save_npz(outdir+'/model'+str(outnum), agent.model)
+    #serializers.save_npz(outdir+'/model'+str(outnum), agent.model)
+    agent.save(outdir+'/model'+str(outnum))
     outnum += 1    
     return evaluator.batch_run_evaluation_episodes(env, agent, n_steps, n_episodes, max_episode_len=max_episode_len, logger=logger)
 

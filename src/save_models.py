4330e279803d43aaa33ac58966114d30dd9d3ed7
honda <honda@ykw.elec.keio.ac.jp>
2020-01-07 12:50:01 +0900

    fix:argments

--- src/save_models.py
+++ src/save_models.py
@@ -20,7 +20,7 @@ def batch_run_evaluation_episodes(
     outdir = logger.name
     serializers.save_npz(outdir+'/model'+str(outnum), agent.model)
     outnum += 1    
-    return evaluator.batch_run_evaluation_episodes
+    return evaluator.batch_run_evaluation_episodes(env, agent, n_steps, n_episodes, max_episode_len=max_episode_len, logger=logger)
 
 evaluator.batch_run_evaluation_episodes = batch_run_evaluation_episodes
 

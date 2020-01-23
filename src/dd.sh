922ca143e62f5579db24a29cd37c3aa1ab84209a
honda <honda@ykw.elec.keio.ac.jp>
2020-01-23 17:37:12 +0900

    steps

--- src/dd.sh
+++ src/dd.sh
@@ -2,11 +2,12 @@
 #env="CartPole-v0"
 env="Acrobot-v1"
 mx=2000
-name="Adata"
 N=8
+name="Adata"
+steps=500
 for ((s=0;s<$N;s+=1));do
     for ((i=$s;i<$mx;i+=$N));do
         echo ${i}${name}
-        python3 -u make_data.py --env $env --outdir $name --gpu $(($i%2)) --seed 0 --load $i;
+        python3 -u make_data.py --env $env --outdir $name --gpu $(($i%2)) --seed 0 --load $i --steps $steps;
     done&
 done;

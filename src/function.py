10f025e9230c120d08342ebc12081b70c14d8101
honda <honda@ykw.elec.keio.ac.jp>
2020-01-23 21:31:51 +0900

    fix: file not exitst

--- src/function.py
+++ src/function.py
@@ -3,6 +3,7 @@ import json, os, glob
 def seed_used(seed, path):
     for dr in glob.glob(path):
         for file in glob.glob(dr+"/*/"):
-            with open(file+"args.txt", 'r') as f:
-                if seed == json.loads(f.read())["seed"]:
-                    return os.path.exists(file+"best")
+            if os.path.exists(file+"args.txt"):
+                with open(file+"args.txt", 'r') as f:
+                    if seed == json.loads(f.read())["seed"]:
+                        return os.path.exists(file+"best")

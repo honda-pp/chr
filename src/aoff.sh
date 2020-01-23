2c41c47a6e27c021e5a8707b26a363ec7de64a30
honda <honda@ykw.elec.keio.ac.jp>
2020-01-23 18:22:46 +0900

    acrobot

---
+++ src/aoff.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+env="Acrobot-v1"
+mx=20
+N=1
+ve_list=(1 4 8 16)
+is_list=(0.01 0.05 0.1 0.5)
+ie_list=(1 4 8 16)
+os_list=(0.01 0.05 0.1 0.5)
+count=0
+for ve in "${ve_list[@]}";do
+    v_learn_epochs=$ve
+    for ie in "${ie_list[@]}";do
+        innerepochs=$ie
+        for os in "${os_list[@]}";do
+            outerstepsize=$os
+            for is in "${is_list[@]}";do
+                innerstepsize=$is
+                for ((s=0;s<$N;s+=1));do
+                    for ((i=$s;i<$mx;i+=N));do
+                        name="a/off-ve"${v_learn_epochs}"-ie"${innerepochs}"-os"${outerstepsize}"-is"${innerstepsize}
+                        echo ${i}${name}                        
+                        python3 -u off_and_perf.py --outdir $name --md_outdir "Atrained" --base_path "adt" --env $env --gpu $(($count%2)) --seed $i --v_learn_epochs $v_learn_epochs --innerepochs $innerepochs --innerstepsize $innerstepsize --outerstepsize $outerstepsize >> "log/${name}.out";
+                    done;
+                done;
+            done;
+        done;
+    done&
+    count=$((++count))
+done;

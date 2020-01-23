ef5af3b5781e4717bc172943b8c79a1d924432d7
honda <honda@ykw.elec.keio.ac.jp>
2020-01-23 15:46:12 +0900

    acrobot

--- src/dd.sh
+++ src/dd.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-env0="CartPole-v0"
+#env="CartPole-v0"
+env="Acrobot-v1"
 mx=2000
 name="data"
 N=8
 for ((s=0;s<$N;s+=1));do
     for ((i=$s;i<$mx;i+=$N));do
         echo ${i}${name}
-        python3 -u make_data.py --outdir $name --gpu $(($i%2)) --seed 0 --load $i;
+        python3 -u make_data.py --env $env --outdir $name --gpu $(($i%2)) --seed 0 --load $i;
     done&
 done;

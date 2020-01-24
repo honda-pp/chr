2ae53fc8ee73bf8381196878ea694537ffe412db
honda <honda@ykw.elec.keio.ac.jp>
2020-01-24 18:00:39 +0900

    var

--- src/norm.sh
+++ src/norm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+N=4
+mx=20
 env="CartPole-v0"
-
 for ((s=0;s<$N;s+=1));do
     for ((i=$s;i<$mx;i+=4));do
         python3 -u pa.py --outdir Cperf/a2c --env $env --gpu $(($i%2)) --seed $i  --steps 20000 --eval-interval 100 > cpa.out; 

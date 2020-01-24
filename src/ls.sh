a775bf2d7ceeddfae447d01f78b300495eec8f88
honda <honda@ykw.elec.keio.ac.jp>
2020-01-24 17:13:58 +0900

    outdir

--- src/ls.sh
+++ src/ls.sh
@@ -9,7 +9,7 @@ for ((s=0;s<$N;s+=1));do
     for ((i=$s;i<$mx;i+=4));do
         for l in "${list[@]}";do
             echo ${i}-$l
-            echo $l |xargs python3 -u off_and_perf.py --outdir Cperf --md_outdir "Ctrained" --base_path "cdt" --env $env --seed $i --gpu 1 #$(($i%2))
+            echo $l |xargs python3 -u off_and_perf.py --outdir Cperf --md_outdir "trained" --base_path "dt" --env $env --seed $i --gpu 1 #$(($i%2))
         done;
     done&
 done;

d1dcd26ac17af4e3a72438295f31cbef4a86c12d
honda <honda@ykw.elec.keio.ac.jp>
2020-01-24 17:19:51 +0900

    change: steps

--- src/ls.sh
+++ src/ls.sh
@@ -9,7 +9,7 @@ for ((s=0;s<$N;s+=1));do
     for ((i=$s;i<$mx;i+=4));do
         for l in "${list[@]}";do
             echo ${i}-$l
-            echo $l |xargs python3 -u off_and_perf.py --outdir Cperf --md_outdir "trained" --base_path "dt" --env $env --seed $i --gpu 1 #$(($i%2))
+            echo $l |xargs python3 -u off_and_perf.py --md_outdir "trained" --base_path "dt" --steps 20000 --eval_interval 100 --env $env --seed $i --gpu 1 #$(($i%2))
         done;
     done&
 done;
